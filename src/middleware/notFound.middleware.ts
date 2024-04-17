495424428dfb22e53cd2fa81b5f0cfd98221efd1
mile-mijatovic <mile@milemijatovic.com>
2024-04-17 14:23:54 +0200

    Changed message for notFoundHandler

--- src/middleware/notFound.middleware.ts
+++ src/middleware/notFound.middleware.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { NotFoundError } from '../errors';
+import messages from '../assets/json/messages.json';
 
 const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
   if (!req.route) {
-    return next(new NotFoundError('Page is not found'));
+    return next(new NotFoundError(messages.notFound));
   }
 };
 
